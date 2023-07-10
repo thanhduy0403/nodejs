9e36d3258c82659c37ddd6d7950e0d34b2510957
thanhduy29 <nguyenthanhduy0429@gmail.com>
2023-07-11 00:32:29 +0700

    update

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const Category = require("./Category");
+const Supplier = require("./Supplier");
+const Product =  require ("./Product")
 module.exports = {
   Category,
-
- 
+  Supplier,
+  Product,
 };
--- routes/admin/routes.js
+++ routes/admin/routes.js
@@ -2,8 +2,11 @@ var express = require ('express')
 const router = express.Router();
 
 const categoryRouter = require('./category/router');
+const supplierRouter = require ('./supplier/router');
+const productRouter = require ('./product/router');
 
 router.use ('/category',categoryRouter);
-
+router.use('/suppliers', supplierRouter);
+router.use('/product', productRouter);
 
 module.exports = router
