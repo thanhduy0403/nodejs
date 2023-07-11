7aa3ff7d01c073717ce4521073d283a50a587b2b
khoinv21it <khoinguyen010503@gmail.com>
2023-07-11 17:58:59 +0700

    commit

---
+++ routes/user/product/router.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const { validateSchema } = require('../../../utils');
+const {
+  getProductSchema,
+  createProductSchema,
+} = require('./validations');
+const {
+  getProductAll,
+  getProductDetail,
+} = require('./controller');
+
+router.route('/').get(getProductAll)
+
+router.route('/:id').get(validateSchema(getProductSchema), getProductDetail)
+
+module.exports = router;
