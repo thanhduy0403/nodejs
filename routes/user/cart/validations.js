bed9e0633d52d802388fccfe4ec75acb271fad72
khoinv21it <khoinguyen010503@gmail.com>
2023-07-16 00:15:10 +0700

    committtt

---
+++ routes/user/cart/validations.js
@@ -0,0 +1,44 @@
+const yup = require('yup');
+const ObjectId = require('mongodb').ObjectId;
+
+module.exports = {
+
+  getDetailSchema: yup.object({
+    params: yup.object({
+      id: yup.string().test('validationID', 'ID sai định dạng', (value) => {
+        return ObjectId.isValid(value);
+      }),
+    }),
+  }),
+
+  removeSchema: yup.object({
+    body: yup.object({
+      customerId: yup.string().test('validationID', 'ID sai định dạng', (value) => {
+        return ObjectId.isValid(value);
+      }),
+      productId: yup.string().test('validationID', 'ID sai định dạng', (value) => {
+        return ObjectId.isValid(value);
+      }),
+    }),
+  }),
+
+  createSchema: yup.object({
+    body: yup.object({
+      customerId: yup
+        .string()
+        .required()
+        .test('validationCustomerID', 'ID sai định dạng', (value) => {
+          return ObjectId.isValid(value);
+        }),
+
+      productId: yup
+        .string()
+        .required()
+        .test('validationProductID', 'ID sai định dạng', (value) => {
+          return ObjectId.isValid(value);
+        }),
+
+      quantity: yup.number().required().min(0),
+    }),
+  }),
+};
--- routes/user/router.js
+++ routes/user/router.js
@@ -1,12 +1,26 @@
 var express = require ('express')
 const router = express.Router();
 
+const passport = require('passport');
+
+const {
+  passportConfigUser,
+  passportConfigLocalUser,
+} = require('../../middle-wares/passportUser');
+
+passport.use('jwtUser', passportConfigUser);
+passport.use('localUser', passportConfigLocalUser);
+
 const categoryRouter = require('./category/router');
 const productRouter = require ('./product/router');
 const customerRouter = require ('./customer/router');
+const employeesRouter = require('./employee/router');
+const ordersRouter = require('./order/router');
 
-router.use ('/category',categoryRouter);
 router.use('/product', productRouter);
-router.use('/customer', customerRouter);
+router.use ('/category', passport.authenticate('jwtUser', { session: false }), categoryRouter);
+router.use('/customer', passport.authenticate('jwtUser', { session: false }), customerRouter);
+router.use('/employees', passport.authenticate('jwtUser', { session: false }), employeesRouter);
+router.use('/orders', passport.authenticate('jwtUser', { session: false }), ordersRouter);
 
 module.exports = router
